78326895c54f3efc109be143c63576b334ea71ad
BrensUwU <162404455+BrensUwU@users.noreply.github.com>
2024-07-29 08:52:42 -0600

    Delete clase 23-07-2024 directory

--- clase 23-07-2024/src/FactorialIterativo.java
+++
@@ -1,9 +0,0 @@
-public class FactorialIterativo {
-    public static int factorial(int n) {
-        int resultado = 1;
-        for (int i = 1; i <= n; i++) {
-            resultado *= i;
-        }
-        return resultado;
-    }
-}
