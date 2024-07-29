e22e24b0c76cf2f4e9e9268ef51efcf5ffb1d33a
BrensUwU <162404455+BrensUwU@users.noreply.github.com>
2024-07-29 08:51:28 -0600

    Add files via upload

---
+++ clase 23-07-2024/src/FactorialIterativo.java
@@ -0,0 +1,9 @@
+public class FactorialIterativo {
+    public static int factorial(int n) {
+        int resultado = 1;
+        for (int i = 1; i <= n; i++) {
+            resultado *= i;
+        }
+        return resultado;
+    }
+}
